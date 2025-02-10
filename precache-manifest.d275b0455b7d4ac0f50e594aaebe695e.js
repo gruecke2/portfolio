25a44666e02432932fb77e0c8e8d4f0b9d5dfb12
gruecke2 <garret.rueckert.guitar@gmail.com>
2025-02-10 15:31:43 -0700

    Updates

---
+++ precache-manifest.d275b0455b7d4ac0f50e594aaebe695e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e045116fcddce2e81466fc94c4372f1f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9d5736da3ed7c331c712",
+    "url": "./static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "99e4b404ef2979a8c22b",
+    "url": "./static/css/main.0f7e6c1a.chunk.css"
+  },
+  {
+    "revision": "9d5736da3ed7c331c712",
+    "url": "./static/js/2.5cb4586f.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "./static/js/2.5cb4586f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99e4b404ef2979a8c22b",
+    "url": "./static/js/main.28adea7b.chunk.js"
+  },
+  {
+    "revision": "1d6896b13d1316b5acb7",
+    "url": "./static/js/runtime-main.2b4c0fc3.js"
+  },
+  {
+    "revision": "7c0183caeb10576cee402a74d40e95f2",
+    "url": "./static/media/myProfile.7c0183ca.jpg"
+  },
+  {
+    "revision": "b5c22c8e0b7934677f3b43691db5ad87",
+    "url": "./static/media/myProfile_old.b5c22c8e.jpg"
+  }
+]);
